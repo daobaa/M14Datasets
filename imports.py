6a737c79a6b36ff1b2aca325704da47642e51025
Diego Andres Olivera Abarca <2024_diego.olivera@iticbcn.cat>
2025-03-06 19:17:33 +0100

    Importing Base Files

---
+++ imports.py
@@ -0,0 +1,15 @@
+# -------------------------------
+# Sección 1: Imports y Carga de Datos
+# -------------------------------
+import pandas as pd
+
+# URL del archivo CSV en GitHub (raw)
+url = "https://raw.githubusercontent.com/daobaa/M14Datasets/master/mxmh_survey_results.csv"
+
+# Leer el archivo CSV desde la URL
+df = pd.read_csv(url)
+
+# -------------------------------
+# Sección 2: Análisis de Datos
+# -------------------------------
+df.head()
