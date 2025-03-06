5f02a4a6f91eac91ea93248d5ded41db341b0611
Diego Andres Olivera Abarca <2024_diego.olivera@iticbcn.cat>
2025-03-06 19:45:43 +0100

    Corrected imports | Testing ipynb

--- imports.py
+++ imports.py
@@ -1,10 +1,6 @@
-# -------------------------------
-# Sección 1: Imports y Carga de Datos
-# -------------------------------
 import pandas as pd
+import matplotlib.pyplot as plt
 
-# URL del archivo CSV en GitHub (raw)
 url = "https://raw.githubusercontent.com/daobaa/M14Datasets/master/mxmh_survey_results.csv"
 
-# Leer el archivo CSV desde la URL
 df = pd.read_csv(url)
